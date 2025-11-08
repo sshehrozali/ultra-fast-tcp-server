44ee94a06452d5d3c1570c7b60537527eca713af
sshehrozali <sshehroza13@gmail.com>
2025-11-08 15:34:46 +0500

    added code

---
+++ src/main/java/com/tcp/CustomChannelHandler.java
@@ -0,0 +1,26 @@
+package com.tcp;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+public class CustomChannelHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in); // Echo back
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/tcp/TcpServer.java
@@ -0,0 +1,35 @@
+package com.tcp;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TcpServer {
+    public static void main(String[] args) throws InterruptedException {
+        var bossGroup = new NioEventLoopGroup(1);
+        var workerGroup = new NioEventLoopGroup();
+
+        try {
+            var server = new ServerBootstrap();
+            server
+                    .group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)  // TCP connection
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new CustomChannelHandler());
+                        }
+                    });
+
+            var channelFuture = server.bind(8000).sync();
+            System.out.println("🚀 TCP Server started on port 8080");
+            channelFuture.channel().closeFuture().sync();
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
